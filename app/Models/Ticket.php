ce047bfda33072cdb998b17c538fd527e6c682d4
Fernando Barajas <jesusfernandobarajasgalindo@gmail.com>
2025-09-07 22:08:12 -0600

    feature: all models were created

---
+++ app/Models/Ticket.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Enums\TicketStatus;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ticket extends Model
+{
+    use HasFactory,HasUlids,SoftDeletes;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'status' => TicketStatus::class,
+        ];
+    }
+}
---
+++ database/factories/TicketNoteFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TicketNote>
+ */
+class TicketNoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
